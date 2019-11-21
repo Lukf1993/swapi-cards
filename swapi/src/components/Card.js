305316b2839c715192e09cfa2df92bb6499c92b5
Łukasz Fundament <lukasz.fundament@almservices.pl>
2019-11-21 17:04:33 +0100

    add toggleClass function

--- swapi/src/components/Card.js
+++ swapi/src/components/Card.js
@@ -5,14 +5,24 @@ const Card = props => {
     
     const people = props.people;
     const addFavorite = props.addFavorite;
-    const getPersonMovie = props.getPersonMovie    
+    const getPersonMovie = props.getPersonMovie;
+    
+    const toggleClass = (item) => {
+      const parent = item.parentElement;
+      const hasClass = parent.classList;
+      
+      if(!item.classList.contains('card__button')) {;
+      if(hasClass.contains('jsReverse')) {
+        parent.classList.toggle('card__item--reverse');
+      }}
+    }
     
     return (
        <div className='card'>
        {people.map(item => (
-          <div className='card__item' key={item.url}>
-            <button  onClick={() => addFavorite(item)}>Ulubione</button>
+          <div className='card__item jsReverse' key={item.url} onClick={(e) => toggleClass(e.target)}>
             <CardContent personMovie={getPersonMovie(item)} item={item} />
+            <button className='card__button' onClick={() => addFavorite(item)}>Add</button>
           </div>
       ))}
       </div> 
